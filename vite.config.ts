541ea652bf25827f4f7aff4b4baff8deb68fa87e
empsf163it-tech <empsf163it@gmail.com>
2026-08-04 23:01:04 +0530

    thirdcommitcheck

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Dropshipping_Store_Website/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
